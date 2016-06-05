d074d1c19fb532a65783d3eca901bf7adb6f173e
Aubrey Keus <aekeus@keus.org>
2016-06-05 02:35:28 +0000

    Allow override of world load filename

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ if (process.env.PROD) {
 
 var config = JSON.parse(fs.readFileSync(path.join('config', configFilename), 'utf-8'))
 
-var worldFilename = path.join('misc', config.world)
+var worldFilename = path.join('misc', process.env.WORLD || config.world)
 
 var k = new kernel.Kernel(config.port)
 k.loadWorld(worldFilename, function() {
